67484f1fbf0eb6f67b43295a73fe2e252b236d7f
jeffreyji <gefforey520@gmail.com>
2014-09-25 21:15:10 +0800

    refactor

--- commerceApp/commerceAndroid/gen/com/intellect/commerce/commerceandroid/BuildConfig.java
+++ commerceApp/commerceAndroid/gen/com/intellect/commerce/commerceandroid/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.intellect.commerce;
+package com.intellect.commerce.commerceandroid;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
