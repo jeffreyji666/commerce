fb720146b95a9c37019078eb1a28182b5a4c7024
unknown <wgji@DST60063.cn1.global.ctrip.com>
2014-09-25 16:08:22 +0800

    add basic util class

--- commerceApp/commerceAndroid/gen/com/intellect/commerce/BuildConfig.java
+++ commerceApp/commerceAndroid/gen/com/intellect/commerce/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.intellect.commerce.commerceandroid;
+package com.intellect.commerce;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
