0b9b4520ee42ed94dafd0f9719d4e4ede1365c1d
gefforey520@gmail.com <gefforey520@gmail.com>
2014-10-16 10:21:47 +0800

    add capture activity

--- commerceApp/app/src/main/java/mobile/intellect/commerceapp/ui/HomeActivity.java
+++ commerceApp/app/src/main/java/mobile/intellect/commerceapp/ui/HomeActivity.java
@@ -72,7 +72,7 @@ public class HomeActivity extends FragmentActivity {
         qrcode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), CommodityActivity.class);
+                Intent intent = new Intent(v.getContext(), CaptureActivity.class);
                 v.getContext().startActivity(intent);
             }
         });
